a7d90481292f5da93c21b3a3e8ca5662df5d3879
Fran <franrocher95@gmail.com>
2024-05-02 18:17:38 +0200

    feat:adding login service to api calls

--- src/services/authApiCalls.js
+++ src/services/authApiCalls.js
@@ -24,5 +24,28 @@ export const registerService = async (user) => {
         return error;
     }
 };
+export const loginService = async (user) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+    };
+
+    try {
+        const response = await fetch(`${root}auth/login`, options);
+
+        const data = await response.json();
+
+        if (!data.success) {
+            throw new Error(data.message);
+        }
+
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
 
 
