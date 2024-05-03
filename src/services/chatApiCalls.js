1d09b8e576ac8b62c4ed02855afac36c209b8a1e
Fran <franrocher95@gmail.com>
2024-05-04 00:12:54 +0200

    feat:adding createChatService in chatApiCalls.js

--- src/services/chatApiCalls.js
+++ src/services/chatApiCalls.js
@@ -23,6 +23,30 @@ export const getUserChatsService = async (token) => {
         return error;
     }
 };
+export const createChatService = async (chat,token) => {
+    const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(chat),
+      };
+
+    try {
+        const response = await fetch(`${root}chats`, options);
+
+        const data = await response.json();
+
+        if (!data.success) {
+            throw new Error(data.message);
+        }
+
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
 export const bringMessagesService = async (chatId,token) => {
     const options = {
         method: "GET",
