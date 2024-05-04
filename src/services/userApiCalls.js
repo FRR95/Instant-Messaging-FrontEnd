c4a364aabe468f416b85bf177262c458860bfee3
Fran <franrocher95@gmail.com>
2024-05-04 23:11:46 +0200

    feature:adding update profile serivce in userApiCalls.js

--- src/services/userApiCalls.js
+++ src/services/userApiCalls.js
@@ -23,3 +23,27 @@ export const GetProfile = async (token) => {
         return error;
     }
 };
+export const updateProfileService = async (profile,token) => {
+    const options = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(profile),
+      };
+
+    try {
+        const response = await fetch(`${root}user/me`, options);
+
+        const data = await response.json();
+
+        if (!data.success) {
+            throw new Error(data.message);
+        }
+
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
