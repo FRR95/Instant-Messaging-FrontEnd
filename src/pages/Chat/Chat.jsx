b916ba5687199279315bf668259354f22b6a963e
Fran <franrocher95@gmail.com>
2024-05-04 13:33:11 +0200

    feat:adding update and delete chat function in Chat page

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { userData } from "../../app/slices/userSlice";
-import { createChatService, getUserChatsService } from "../../services/chatApiCalls";
+import { createChatService, deleteChatService, getUserChatsService, updateChatService } from "../../services/chatApiCalls";
 import { updateChatDetail } from "../../app/slices/chatDetailSlice";
 import { CustomInput } from "../../components/CustomInput/CustomInput";
 import { validation } from "../../utils/validations";
@@ -17,6 +17,7 @@ export const Chat = () => {
     const [chat, setChat] = useState([]);
 
     const [chatCredential, setchatCredential] = useState({
+        id:"",
         name: "",
     });
 
@@ -83,7 +84,7 @@ export const Chat = () => {
     const createChat = async () => {
         try {
 
-            const fetched = await createChatService(chatCredential,rdxUser?.credentials?.token)
+            const fetched = await createChatService(chatCredential, rdxUser?.credentials?.token)
 
             if (!fetched.success) {
                 console.log(fetched.message)
@@ -95,6 +96,55 @@ export const Chat = () => {
 
         }
     }
+
+    const deleteChat = async (chatId) => {
+        try {
+            const fetched = await deleteChatService(chatId, rdxUser?.credentials?.token)
+            if (!fetched.success) {
+                console.log(fetched.message)
+            }
+            console.log(fetched.message)
+            getUserChats()
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const updateChat = async (chatId) => {
+
+     
+        const fetched = await updateChatService(chatId, chatCredential, rdxUser.credentials.token)
+        if (!fetched.success) {
+            console.log(fetched.message)
+        }
+    
+        console.log(fetched.message)
+        getUserChats()
+    
+        setchatCredential({
+          id: "",
+          name: "",
+        })
+      }
+
+    const AddInfoToForm = async (chat) => {
+        setchatCredential({
+          id: chat.id,
+          name: chat.name,
+         
+        })
+      }
+
+      const clearForm = async () => {
+        setchatCredential({
+          id: "",
+          name: "",
+        })
+    
+    
+      }
+
 
 
     return (
@@ -130,15 +180,46 @@ export const Chat = () => {
                     </div>
                 </div>
 
+                <div className="modal fade " id="updateChatModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog ">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h3 className="modal-title fs-5" id="exampleModalLabel">Editar chat</h3>
+                                <button type="button" onClick={clearForm} className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+
+
+                                <CustomInput
+                                    type="text"
+                                    name="name"
+                                    placeholder={"Nombre del nuevo grupo"}
+                                    value={chatCredential.name || ""}
+                                    changeEmit={inputHandler}
+                                    onBlurFunction={(e) => checkError(e)}
+                                />
+                                <div className="error">{credencialesError.nameError}</div>
+                            </div>
+                            <div className="modal-footer">
+
+                                <button type="button" onClick={()=>updateChat(chatCredential.id)} className="btn buttonEditDesign " data-bs-dismiss="modal"><i className="bi bi-plus-square"></i>{`Editar chat`}</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 {chat.length > 0
                     ? (<>{chat.map(chats => {
                         return (
                             <>
+                            {/* onClick={() => manageChatDetail(chats)} */}
                                 <div className="d-flex row mb-1 justify-content-center align-items-center chatCardSectionDesign ">
-                                    <div className="d-flex row justify-content-center align-items-center chatCardDesign" onClick={() => manageChatDetail(chats)}>
-                                        <div className="d-flex col justify-content-center align-items-center">
-                                            <div className="d-flex col-6 justify-content-center align-items-center">{chats.name}</div>
-                                            <div className="d-flex col-6 justify-content-center align-items-center">{chats.author_id}</div>
+                                    <div className="d-flex row justify-content-center align-items-center chatCardDesign" > 
+                                        <div className="d-flex col justify-content-center align-items-center" onClick={() => manageChatDetail(chats)}>
+                                            <div className={chats.author_id === rdxUser?.credentials?.profileDetail?.id ? ("d-flex col-3 justify-content-center align-items-center") : ("d-flex col-6 justify-content-center align-items-center")}>{chats.name}</div>
+                                            <div className={chats.author_id === rdxUser?.credentials?.profileDetail?.id ? ("d-flex col-3 justify-content-center align-items-center") : ("d-flex col-6 justify-content-center align-items-center")}>{chats.author_id}</div>
+                                            <div className={chats.author_id === rdxUser?.credentials?.profileDetail?.id ? ("d-flex col-3 justify-content-center align-items-center") : ("d-none")}><button onClick={() => deleteChat(chats.id)}><i className="bi bi-trash"></i></button></div>
+                                            <div className={chats.author_id === rdxUser?.credentials?.profileDetail?.id ? ("d-flex col-3 justify-content-center align-items-center") : ("d-none")}><button data-bs-toggle="modal" data-bs-target="#updateChatModal" onClick={() => AddInfoToForm(chats)}><i class="bi bi-pencil"></i></button></div>
 
                                         </div>
                                     </div>
