65dbaf551a8022e6dfbadc4a0aace5a73ba5662b
Fran <franrocher95@gmail.com>
2024-05-04 13:32:09 +0200

    feat:adding create and update chat function in chatApiCalls.js

--- src/services/chatApiCalls.js
+++ src/services/chatApiCalls.js
@@ -47,6 +47,53 @@ export const createChatService = async (chat,token) => {
         return error;
     }
 };
+export const deleteChatService = async (chatId,token) => {
+    const options = {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        }
+      };
+
+    try {
+        const response = await fetch(`${root}chats/${chatId}`, options);
+
+        const data = await response.json();
+
+        if (!data.success) {
+            throw new Error(data.message);
+        }
+
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
+export const updateChatService = async (chatId,chat,token) => {
+    const options = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify(chat),
+      };
+
+    try {
+        const response = await fetch(`${root}chats/${chatId}`, options);
+
+        const data = await response.json();
+
+        if (!data.success) {
+            throw new Error(data.message);
+        }
+
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
 export const bringMessagesService = async (chatId,token) => {
     const options = {
         method: "GET",
