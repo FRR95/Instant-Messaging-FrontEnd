28d0e6dd64295139bdb3f178186e0c76067145b8
Fran <franrocher95@gmail.com>
2024-05-05 12:41:23 +0200

    feat:adding getUser and deleteUser functions in userApiCalls.js

--- src/services/userApiCalls.js
+++ src/services/userApiCalls.js
@@ -47,3 +47,49 @@ export const updateProfileService = async (profile,token) => {
         return error;
     }
 };
+export const getUsersService = async (token) => {
+    const options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        }
+      };
+
+    try {
+        const response = await fetch(`${root}users`, options);
+
+        const data = await response.json();
+
+        if (!data.success) {
+            throw new Error(data.message);
+        }
+
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
+export const deleteUserService = async (userId,token) => {
+    const options = {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        }
+      };
+
+    try {
+        const response = await fetch(`${root}users/${userId}`, options);
+
+        const data = await response.json();
+
+        if (!data.success) {
+            throw new Error(data.message);
+        }
+
+        return data;
+    } catch (error) {
+        return error;
+    }
+};
